162b3fc25216a52a28d5056ee74a9b2fdd6e9e22
sboudfor <sboudfor@gmail.com>
2019-12-24 16:14:55 +0100

    B4AP-8 : fix navigation

--- src/app/b4-expenses/b4-expenses.module.ts
+++ src/app/b4-expenses/b4-expenses.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IncomeScreenComponent } from './income-screen/income-screen.component';
 import {B4LibModule} from '../b4-lib/b4-lib.module';
 import {IncomeRepository} from './services/IncomeRepository';
+import {MaterialModule} from './MaterialModule';
 
 
 
@@ -10,6 +11,7 @@ import {IncomeRepository} from './services/IncomeRepository';
   declarations: [IncomeScreenComponent],
   imports: [
     CommonModule,
+    MaterialModule,
     B4LibModule
   ],
   exports: [IncomeScreenComponent],
